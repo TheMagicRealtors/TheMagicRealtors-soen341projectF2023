6291e45a86866e4cc04173efbf2616dbd3857b4c
midhurshaan <midhur-2002@hotmail.com>
2023-11-10 20:55:00 -0500

    Worked on the submit offer button

--- Sprint2/property page/property.php
+++ Sprint2/property page/property.php
@@ -1,12 +1,17 @@
 <?php
+// session_start();
+
 include 'property_functions.php';
+
 require 'header.php';
+require 'login_functions.php';
 
 if (isset($_GET['address'])) {
     $selectedAddress = $_GET['address'];
 }
 
 $property = getPropertyFromAddress($selectedAddress);
+
 
 $garage = '';
 if($property['garage'] == 0){
@@ -36,6 +41,11 @@ if($property['garage'] == 0){
     echo '</div>';
     echo '<div style="background-color: rgb(255, 255, 255); padding: 30px;">';
     echo '<button class="btn btn-outline-light" style="background-color: #000080;" onclick="saveVisitAddress(\'' . $property['address'] . '\')">Book a Visit</button>';
+    echo $user['user_type'];
+    if($userType == '2'){
+        echo '<button class="btn btn-outline-light" style="background-color: #000080;" onclick="submitOffer()">Make Offer</button>';
+    }
+    
     echo '</div>';
     echo '</div>';
 
--- Sprint2/property page/property_functions.php
+++ Sprint2/property page/property_functions.php
@@ -1,16 +1,17 @@
 <?php
-function pdo_connect_mysql() { 
-    $DATABASE_HOST = 'localhost'; 
-    $DATABASE_USER = 'root'; 
-    $DATABASE_PASS = ''; 
-    $DATABASE_NAME = 'magicrealtors'; 
-    try { 
-        return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS); 
-    } catch (PDOException $exception) { 
-        exit('Failed to connect to database!'); 
-    } 
-}
-$pdo = pdo_connect_mysql();
+require 'pdoConnect.php';
+// function pdo_connect_mysql() { 
+//     $DATABASE_HOST = 'localhost'; 
+//     $DATABASE_USER = 'root'; 
+//     $DATABASE_PASS = ''; 
+//     $DATABASE_NAME = 'magicrealtors'; 
+//     try { 
+//         return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS); 
+//     } catch (PDOException $exception) { 
+//         exit('Failed to connect to database!'); 
+//     } 
+// }
+// $pdo = pdo_connect_mysql();
 
 function getProperty(){
     global $pdo; 
@@ -25,6 +26,12 @@ function getPropertyFromAddress($address){
     $statement->execute([$address]);
     return $statement->fetch(PDO::FETCH_ASSOC);
 }
+// function getUserType($userType){
+//     global $pdo;
+//     $statement = $pdo->prepare('SELECT * FROM users where user_type = :user_type');
+//     $statement->execute([$userType]);
+//     return $statement->fetch(PDO::FETCH_ASSOC);
+// }
 
 
 ?>
