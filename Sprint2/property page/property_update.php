a850830f96a30e292ca5b8ad20ec61c64dd88a18
Christa <82023874+christa-ux@users.noreply.github.com>
2023-11-27 00:13:28 -0500

    Update property_update.php

--- Sprint2/property page/property_update.php
+++ Sprint2/property page/property_update.php
@@ -6,7 +6,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["updateProperty"])) {
     $properties_id = $_POST['properties_id'];
     $newPrice = $_POST['price'];
     $newDescription = $_POST['description'];
-    $newImage=$_POST['image_url'];
+   
 
     include 'property_functions.php';
 
@@ -21,14 +21,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["updateProperty"])) {
     if (!empty($newDescription)) {
         $existingPropertyData['description'] = $newDescription;
     }
-    if (!empty($newImage)) {
-        $existingPropertyData['image_url'] = $newImage;
-    }
+   
     
 
     
     // Perform the update with the modified data
-    if (updateProperty($properties_id, $existingPropertyData['price'], $existingPropertyData['description'], $existingPropertyData['image_url'])) {
+    if (updateProperty($properties_id, $existingPropertyData['price'], $existingPropertyData['description'])) {
         header("Location: properties.php");
         exit();
     } else {
