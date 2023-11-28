dad0d41611366f998f698c93b9368c31a7a1f434
Christa <82023874+christa-ux@users.noreply.github.com>
2023-11-28 11:36:49 -0500

    Update book_visit.php

--- Sprint2/property page/book_visit.php
+++ Sprint2/property page/book_visit.php
@@ -9,14 +9,14 @@ if (isset($_GET['address'])) {
 $property = getPropertyFromAddress($selectedAddress);
 
 echo '<img src="'.$property['image_url']. '" class="d-block w-100" alt="House Image">';
-echo '<h4> Book a Visit to <b>' . $property['address'] . ', '. $property['district'] . ', ' . $property['city'] . '</b> now!</h4>';
+echo '<h4 class="ml-2 mt-2"> Book a Visit to <b>' . $property['address'] . ', '. $property['district'] . ', ' . $property['city'] . '</b> now!</h4>';
 
 echo '<div style="font: 95% Arial, Helvetica, sans-serif; padding: 16px;background: #F7F7F7;">
         <form method="post" action="processing_booking.php">
             <input type="text" id="email" name="email" placeholder="Email Address" />
             <input type="hidden" name="address" value="' . htmlspecialchars($selectedAddress) . '">
             <br><br>
-            <textarea name="availabilities" id="availabilities" placeholder="Type your Availabilities" required></textarea>
+            <textarea name="availabilities" id="availabilities" placeholder="Type your Availabilities" required></textarea> <br>
 <button class="btn btn-outline-light" style="background-color: #000080;">Submit</button>
 </form>
 </div>';
